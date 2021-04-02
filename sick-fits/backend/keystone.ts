d855a823de8de7d35365cb3830c6ccce81438a67
brandonperfetti <brandon@brandonperfetti.com>
2021-04-02 13:21:14 -0700

    Configure auth.

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,7 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { User } from './schemas/User';
+import 'dotenv/config';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -10,25 +11,37 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: () => true,
+    },
+    // TODO: Add session values here
+  })
+);
