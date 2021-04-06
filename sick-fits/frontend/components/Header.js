238ea54e8106b3b511d5360cc42fc396cc3194f1
brandonperfetti <brandon@brandonperfetti.com>
2021-04-05 18:40:25 -0700

    Fix and style Nav.

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -36,15 +36,15 @@ const HeaderStyles = styled.header`
 export default function Header() {
   return (
     <HeaderStyles>
-      <div className="bar">
+      <div className='bar'>
         <Logo>
-          <Link href="/">Sick fits</Link>
+          <Link href='/'>Sick fits</Link>
         </Logo>
+        <Nav />
       </div>
-      <div className="sub-bar">
+      <div className='sub-bar'>
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
