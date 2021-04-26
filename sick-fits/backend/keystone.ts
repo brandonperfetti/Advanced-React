07b8a478b6df7a33a0a3ba66d0e9f700b8bfb6d8
brandonperfetti <brandon@brandonperfetti.com>
2021-04-26 12:30:26 -0700

    Create Order and OrderItem Data Types.

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -12,6 +12,8 @@ import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations/index';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -61,6 +63,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order,
     }),
     extendGraphqlSchema,
     ui: {
