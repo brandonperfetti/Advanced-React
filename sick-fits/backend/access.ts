906109229b7393e7069bb6067df9752b58c1827d
brandonperfetti <brandon@brandonperfetti.com>
2021-04-26 17:28:21 -0700

    Permissions Access Functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,28 @@
 // At it's simplest, access control returns a yes or a no value depending on the users session
 
 import { ListAccessArgs } from './types';
+import { permissionsList } from './schemas/fields';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// permissions check if someone meets a criteria - yes or no.
+export const Permissions = {
+  ...generatedPermissions,
+  // we can also add additional permissions not in UI
+  isAwesome({ session }: ListAccessArgs) {
+    return session?.data.name?.includes('brandon');
+  },
+};
+
+// Rule based function
