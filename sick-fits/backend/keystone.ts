c22109cdb1ce34f4f952c044fde9ac161ce8756a
brandonperfetti <brandon@brandonperfetti.com>
2021-04-26 17:01:05 -0700

    Create Roles and Permissions Schema + UI.

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -14,6 +14,7 @@ import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations/index';
 import { OrderItem } from './schemas/OrderItem';
 import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -65,6 +66,7 @@ export default withAuth(
       CartItem,
       OrderItem,
       Order,
+      Role,
     }),
     extendGraphqlSchema,
     ui: {
