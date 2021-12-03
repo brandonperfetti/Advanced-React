8110849a8bb38dee198d07a35e5cedbc00fc65dd
brandonperfeti <brandon@brandonperfetti.com>
2021-12-03 15:49:21 -0800

    Create Gated Sign In components.

---
+++ sick-fits/frontend/components/RequireSignIn.js
@@ -0,0 +1,8 @@
+import { useUser } from './User';
+import SignIn from './SignIn';
+
+export default function ({ children }) {
+  const me = useUser();
+  if (!me) return null;
+  return children;
+}
