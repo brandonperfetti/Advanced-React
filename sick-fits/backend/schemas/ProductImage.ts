f9b1729243d73c1c66191b8344d3bd9903d5edaa
brandonperfeti <brandon@brandonperfetti.com>
2021-12-03 15:02:56 -0800

    Product Image Permissions.

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,6 +1,7 @@
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { isSignedIn, permissions } from '../access';
 
 // console.log('--------------------------');
 // console.log(process.env.CLOUDINARY_CLOUD_NAME);
@@ -13,6 +14,12 @@ export const cloudinary = {
 };
 
 export const ProductImage = list({
+  access: {
+    create: isSignedIn,
+    read: () => true,
+    update: permissions.canManageProducts,
+    delete: permissions.canManageProducts,
+  },
   fields: {
     image: cloudinaryImage({
       cloudinary,
