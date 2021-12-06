e61b79fdb3a45c4e5a4433567f7d13994c0b1e28
brandonperfeti <brandon@brandonperfetti.com>
2021-12-06 07:23:39 -0800

    Try shiting down to NextJS 10, remove .eslintrc.json, and re install node modules.

--- sick-fits/frontend/components/RequireSignIn.js
+++ sick-fits/frontend/components/RequireSignIn.js
@@ -1,7 +1,7 @@
 import { useUser } from './User';
 import SignIn from './SignIn';
 
-export default function ({ children }) {
+export default function RequireSignIn({ children }) {
   const me = useUser();
   if (!me) return null;
   return children;
